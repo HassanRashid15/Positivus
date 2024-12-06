12b256ab6c127171c76faa167d37274a9d4993f7
Abdullah Irfan <hassanrashid0018@gmail.com>
2024-12-06 18:51:53 +0500

    New Changes Added

--- src/component/Contact.js
+++ src/component/Contact.js
@@ -93,7 +93,7 @@ function Contact() {
                   type="submit"
                   className="w-full px-4 py-2 text-lg font-semibold text-white transition-colors duration-300 bg-blue-500 rounded-md shadow hover:bg-blue-600 focus:outline-none focus:ring-blue-200 focus:ring-4"
                 >
-                  Send Message
+                  Send Messages
                 </button>
               </div>
             </form>
